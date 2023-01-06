9e9267c8d1146e3f44dea5b87bdf7ccfce88cadf
TomOr37 <81587688+TomOr37@users.noreply.github.com>
2023-01-06 09:43:19 +0200

    refractor some of the code. working.
    need to update readme

--- birdServices.py
+++ birdServices.py
@@ -1,24 +1,12 @@
-"""
-The classic game of flappy bird. Make with python
-and pygame. Features pixel perfect collision using masks :o
-
-"""
 import pygame
-import random
 import os
-import time
-from eckity.algorithms.simple_evolution import SimpleEvolution
-from eckity.breeders.simple_breeder import SimpleBreeder
-from eckity.genetic_operators.selections.tournament_selection import TournamentSelection
-from eckity.statistics.best_average_worst_statistics import BestAverageWorstStatistics
-from eckity.subpopulation import Subpopulation
-from eckity.termination_checkers.threshold_from_target_termination_checker import ThresholdFromTargetTerminationChecker
 from eckity.evaluators.simple_individual_evaluator import SimpleIndividualEvaluator
 from eckity.creators.creator import Creator
 from eckity.fitness.simple_fitness import SimpleFitness
 from feedForwardModel import FFModel
 from eckity.individual import Individual
-from operators import ModelAddDistMutation, ModelParamSwapCrossOver
+from gameObjects import Pipe , Base
+from gameUtils import draw_window , blitRotateCenter
 
 pygame.font.init()  # init font
 
@@ -27,16 +15,13 @@ WIN_HEIGHT = 800
 FLOOR = 730
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 END_FONT = pygame.font.SysFont("comicsans", 70)
-DRAW_LINES = False
+
 
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
-pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")).convert_alpha())
-bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "bg.png")).convert_alpha(), (600, 900))
 bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird" + str(x) + ".png"))) for x in
                range(1, 4)]
-base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")).convert_alpha())
 
 IMGS = bird_images
 """
@@ -159,178 +144,6 @@ def getImage(bird: Bird):
     return img
 
 
-class Pipe():
-    """
-    represents a pipe object
-    """
-    GAP = 200
-    VEL = 5
-
-    def __init__(self, x):
-        """
-        initialize pipe object
-        :param x: int
-        :param y: int
-        :return" None
-        """
-        self.x = x
-        self.height = 0
-
-        # where the top and bottom of the pipe is
-        self.top = 0
-        self.bottom = 0
-
-        self.PIPE_TOP = pygame.transform.flip(pipe_img, False, True)
-        self.PIPE_BOTTOM = pipe_img
-
-        self.passed = False
-
-        self.set_height()
-
-    def set_height(self):
-        """
-        set the height of the pipe, from the top of the screen
-        :return: None
-        """
-        self.height = random.randrange(50, 450)
-        self.top = self.height - self.PIPE_TOP.get_height()
-        self.bottom = self.height + self.GAP
-
-    def move(self):
-        """
-        move pipe based on vel
-        :return: None
-        """
-        self.x -= self.VEL
-
-    def draw(self, win):
-        """
-        draw both the top and bottom of the pipe
-        :param win: pygame window/surface
-        :return: None
-        """
-        # draw top
-        win.blit(self.PIPE_TOP, (self.x, self.top))
-        # draw bottom
-        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
-
-    def collide(self, bird, win):
-        """
-        returns if a point is colliding with the pipe
-        :param bird: Bird object
-        :return: Bool
-        """
-        bird_mask = bird.get_mask()
-        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
-        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
-        top_offset = (self.x - bird.x, self.top - round(bird.y))
-        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
-
-        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
-        t_point = bird_mask.overlap(top_mask, top_offset)
-
-        if b_point or t_point:
-            return True
-
-        return False
-
-
-class Base:
-    """
-    Represnts the moving floor of the game
-    """
-    VEL = 5
-    WIDTH = base_img.get_width()
-    IMG = base_img
-
-    def __init__(self, y):
-        """
-        Initialize the object
-        :param y: int
-        :return: None
-        """
-        self.y = y
-        self.x1 = 0
-        self.x2 = self.WIDTH
-
-    def move(self):
-        """
-        move floor so it looks like its scrolling
-        :return: None
-        """
-        self.x1 -= self.VEL
-        self.x2 -= self.VEL
-        if self.x1 + self.WIDTH < 0:
-            self.x1 = self.x2 + self.WIDTH
-
-        if self.x2 + self.WIDTH < 0:
-            self.x2 = self.x1 + self.WIDTH
-
-    def draw(self, win):
-        """
-        Draw the floor. This is two images that move together.
-        :param win: the pygame surface/window
-        :return: None
-        """
-        win.blit(self.IMG, (self.x1, self.y))
-        win.blit(self.IMG, (self.x2, self.y))
-
-
-def blitRotateCenter(surf, image, topleft, angle):
-    """
-    Rotate a surface and blit it to the window
-    :param surf: the surface to blit to
-    :param image: the image surface to rotate
-    :param topLeft: the top left position of the image
-    :param angle: a float value for angle
-    :return: None
-    """
-    rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=topleft).center)
-
-    surf.blit(rotated_image, new_rect.topleft)
-
-
-def draw_window(win, birds, pipes, base, score, pipe_ind):
-    """
-    draws the windows for the best fitted at the end of the algorithm
-    :param win: pygame window surface
-    :param bird: a Bird object
-    :param pipes: List of pipes
-    :param score: score of the game (int)
-    :param pipe_ind: index of closest pipe
-    :return: None
-    """
-    win.blit(bg_img, (0, 0))
-
-    for pipe in pipes:
-        pipe.draw(win)
-
-    base.draw(win)
-    for bird in birds:
-        # draw lines from bird to pipe
-        if DRAW_LINES:
-            try:
-                pygame.draw.line(win, (255, 0, 0),
-                                 (bird.x + bird.img.get_width() / 2, bird.y + bird.img.get_height() / 2),
-                                 (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_TOP.get_width() / 2, pipes[pipe_ind].height),
-                                 5)
-                pygame.draw.line(win, (255, 0, 0),
-                                 (bird.x + bird.img.get_width() / 2, bird.y + bird.img.get_height() / 2), (
-                                     pipes[pipe_ind].x + pipes[pipe_ind].PIPE_BOTTOM.get_width() / 2,
-                                     pipes[pipe_ind].bottom), 5)
-            except:
-                pass
-        # draw bird
-        bird.draw(win)
-
-    # score
-    score_label = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
-    win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
-
-    pygame.display.update()
-
-
 class BirdEvaluator(SimpleIndividualEvaluator):
     def _evaluate_individual(self, individual: Bird):
         return eval(individual, show_game=False)
@@ -395,7 +208,7 @@ def eval(individual: Bird, limit=300, show_game=True):
             # check for collision
             if pipe.collide(individual, win):
                 curr_fitness -= 1
-                #print(curr_fitness)
+                print(curr_fitness)
                 return curr_fitness  # lost game but collided
 
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
@@ -416,12 +229,12 @@ def eval(individual: Bird, limit=300, show_game=True):
 
         if individual.y + getImage(
                 individual).get_height() - 10 >= FLOOR or individual.y < -50:  # indiviual escapes from screen
-           # print(curr_fitness)
+            print(curr_fitness)
             return curr_fitness  # lost game
 
         if curr_fitness > limit:
             return curr_fitness
-        print(curr_fitness)
+
         if show_game:
             draw_window(WIN, [individual], pipes, base, score, pipe_ind)
 
@@ -445,39 +258,3 @@ class BirdCreator(Creator):
         return individuals
 
 
-def main():
-    algo = SimpleEvolution(
-        Subpopulation(creators=BirdCreator(init_pos=(230, 350)),
-                      population_size=80,
-                      # user-defined fitness evaluation method
-                      evaluator=BirdEvaluator(),
-                      # maximization problem (fitness is sum of values), so higher fitness is better
-                      higher_is_better=True,
-                      elitism_rate=1 / 300,
-                      # genetic operators sequence to be applied in each generation
-                      operators_sequence=[
-                          ModelParamSwapCrossOver(probability=0.1),
-                          ModelAddDistMutation(probability=0.1)
-                      ],
-                      selection_methods=[
-                          # (selection method, selection probability) tuple
-                          (TournamentSelection(tournament_size=3, higher_is_better=True), 1)
-                      ]
-                      ),
-        breeder=SimpleBreeder(),
-        max_workers=4,
-        max_generation=5,
-
-        termination_checker=ThresholdFromTargetTerminationChecker(optimal=300, threshold=0.0),
-        statistics=BestAverageWorstStatistics()
-    )
-
-    # evolve the generated initial population
-    algo.evolve()
-
-    # Execute (show) the best solution
-    print(algo.execute())
-
-
-if __name__ == '__main__':
-    main()
